861fafecd3ff19809d2208066e4096bf8245d0cf
David Cowden <dcow90@gmail.com>
2014-05-26 18:19:52 -0700

    Urbit (formula)

---
+++ urbit.rb
@@ -0,0 +1,29 @@
+require 'formula'
+
+class Urbit < Formula
+    homepage "https://urbit.org"
+    head "https://github.com/urbit/urbit.git"
+
+    depends_on 'gmp'
+    depends_on 'libsigsegv'
+    depends_on 'openssl'
+
+    depends_on 'libtool'  => :build
+    depends_on 'autoconf' => :build
+    depends_on 'automake' => :build
+    depends_on 'cmake'    => :build
+
+    def install
+        ENV.deparallelize
+        system "make", "LIB=#{share}"
+        bin.install 'bin/vere'
+        mkdir share
+        mkdir share/'zod'
+        cp   'urb/urbit.pill', share
+        cp_r 'urb/zod/.', share/'zod'
+    end
+
+    test do
+        system 'vere'
+    end
+end
