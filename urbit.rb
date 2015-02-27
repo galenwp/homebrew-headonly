9158bc6e6c42e974ae5d4c20dd6b3adbb1df4f72
David Cowden <david.w.cowden@gmail.com>
2015-02-27 01:06:36 -0800

    Dig up whatever I had last year

--- urbit.rb
+++ urbit.rb
@@ -9,9 +9,17 @@ class Urbit < Formula
     depends_on 'libsigsegv'
     depends_on 'openssl'
 
-    def install 
-        system 'make'
+    def install
+        ENV.deparallelize
+        system "make", "LIB=#{lib}/urb"
+
         bin.install 'bin/vere'
+        mkdir_p lib/'urb/zod'
+        (lib/'urb').cp stage/'urb/urbit.pill'
+        (lib/'urb/zod').cp Dir[stage/'urb/zod/*']
+    end
+
+    test do
+        system 'vere'
     end
 end
-
